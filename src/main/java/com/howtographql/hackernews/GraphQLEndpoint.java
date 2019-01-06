aa37b971a7510b5858e3ed2510a696ffbd99faaf
raj <raj.julha@gmail.com>
2019-01-07 00:11:23 +0400

    Initial running version
    mvn jetty:run
    And open link  http://localhost:8080/graphql?query={allLinks{url}}
    
    Signed-off-by: raj <raj.julha@gmail.com>

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,29 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.GraphQLContext;
+import graphql.servlet.SimpleGraphQLServlet;
+
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
